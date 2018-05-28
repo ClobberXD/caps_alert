1e3e93b1608833df6a78cab5cbfdeb72eaacb055
Anand S <36130650+ClobberXD@users.noreply.github.com>
2018-05-28 12:03:26 +0530

    Major bug-fix, minor typo-fixes, code clean-up

--- init.lua
+++ init.lua
@@ -45,24 +45,25 @@ end
 
 minetest.register_on_chat_message(function(name, message)
 	if (checkMessages(message) == true) and (message:len() > 4) then
-		minetest.show_formspec(name, "caps_alert:warning",
-					"size[7.5,3]" ..
-					"label[1.5,0.5;Stop using excessive CAPS!]" ..
-					"label[0.75,1;You will be kicked if you don't stop using CAPS.]" ..
-					"button_exit[1.5,2;4,1;exit;OK]"
-		)
 		
 		violations[name] = (violations[name] or 0) + 1
 		if violations[name] > 3 then
+			minetest.kick_player(name, "Use of excessive CAPS")
 			for _, player in pairs(minetest.get_connected_players()) do
-				minetest.kick_player(name, "Use of excessive CAPS")
-				
 				local moderator = player:get_player_name()
-				if minetest.check_player_privs(player_name, {kick = true, ban = true}) then
+				if minetest.check_player_privs(moderator, {kick = true, ban = true}) then
 					minetest.chat_send_player(player_name, minetest.colorize("#FFFF00",
-							"-!- " .. name .. " has been kicked for use if excessive CAPS."))
+						"-!- caps_alert: " ..name.. " has been kicked for use of excessive CAPS.")
 				end
 			end
+		end
+			
+		if minetest.get_player_by_name(name) then
+			minetest.show_formspec(name, "caps_alert:warning",
+							"size[7.5,3]" ..
+							"label[1.5,0.5;Stop using excessive CAPS!]" ..
+							"label[0.75,1;You will be kicked if you don't stop using CAPS.]" ..
+							"button_exit[1.5,2;4,1;exit;OK]")
 		end
 		
 		return true
